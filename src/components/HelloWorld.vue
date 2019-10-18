5dbf069011bb853d17dc6c230a9155fa5c78a9c4
MauriceLee <ee.maurice.lee@gmail.com>
2019-10-18 11:57:42 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="yilan">
+    <b-container>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">類型</th>
+            <th scope="col">餐廳名稱</th>
+            <th scope="col">距離民宿車程</th>
+            <th scope="col">食記</th>
+            <th scope="col">菜單</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">桌菜</th>
+            <td>駿懷舊餐廳</td>
+            <td>15mins</td>
+            <td><a href="https://nienie.tw/chunrestaurant/">食記連結</a></td>
+            <td><a href="http://www.eland-chun.com.tw/luodong/5-1.htm">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">大眾美食 (不確定可否定位)</th>
+            <td>米家庒</td>
+            <td>15mins</td>
+            <td><a href="https://candicecity.com/34271/">食記連結</a></td>
+            <td><a href="https://pic.pimg.tw/fresh438/1490759037-3248790178_l.jpg">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">大眾美食 (不確定可否定位)</th>
+            <td>知足豬腳</td>
+            <td>15mins</td>
+            <td><a href="https://kafkalin.com/zhizu/">食記連結</a></td>
+            <td><a href="https://lh3.googleusercontent.com/iQ118wLDEp6dYI-U-wb0ZBaUJHmfXH6-hp41t7ody-v3UJgfvW0ksv5km9YUNue-Hmy3DCfM=w600-h0">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">義式料理</th>
+            <td>3步5食 (多人推薦)</td>
+            <td>15mins</td>
+            <td><a href="https://kafkalin.com/avolte35/">食記連結</a></td>
+            <td><a href="https://kafkalin.com/avolte35/2/">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">義式料理</th>
+            <td>哲屋義大利餐廳</td>
+            <td>20mins</td>
+            <td><a href="https://kafkalin.com/2017-05-20-126/">食記連結</a></td>
+            <td><a href="https://imgur.com/a/uEqFQ#lEWKvyy">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">日式簡餐</th>
+            <td>伊丼</td>
+            <td>15mins</td>
+            <td><a href="https://www.walkerland.com.tw/article/view/220249?page=full">食記連結</a></td>
+            <td><a href="https://img.kafkalin.com/uploads/20190419170647_43.jpg">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">日式簡餐</th>
+            <td>日暮</td>
+            <td>10mins</td>
+            <td><a href="https://saliha.pixnet.net/blog/post/466766477">食記連結</a></td>
+            <td><a href="https://farm8.staticflickr.com/7848/45770249805_3314cd43bd_b.jpg">菜單連結</a></td>
+          </tr>
+          <tr>
+            <th scope="row">日式料理</th>
+            <td>松滿緣</td>
+            <td>10mins</td>
+            <td><a href="https://kafkalin.com/pinefood/">食記連結</a></td>
+            <td><a href="https://img.kafkalin.com/uploads/20171206171139_48.jpg">菜單連結1</a><br><a href="https://img.kafkalin.com/uploads/20171206171149_62.jpg">菜單連結2</a><br><a href="https://img.kafkalin.com/uploads/20171206171151_82.jpg">菜單連結3</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </b-container>
+  </div>
+</template>
